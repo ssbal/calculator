0d3e4d7b5ec7202ef21c78b7945f22e381f278a6
Sukhdeep <sukhdeep29723@gmail.com>
2023-08-30 10:22:15 +0530

    Create basic math functions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Connected!');
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
