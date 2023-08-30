35aa9d8a8ac049d6be9b2986ec1c491f01229754
Sukhdeep <sukhdeep29723@gmail.com>
2023-08-30 10:55:57 +0530

    Update display for the value entered

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.numbers > button');
+let subject = 0;
+
 const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
 const mul = (a, b) => a * b;
@@ -10,7 +14,7 @@ const div = (a, b) => a / b;
 function operate(op, num1, num2) {
   n1 = Number(num1);
   n2 = Number(num2);
-  let result = 0;
+  let result;
 
   switch (op) {
     case '+':
@@ -25,7 +29,18 @@ function operate(op, num1, num2) {
     case '/':
       result = div(n1, n2);
       break;
+    default:
+      result = 0;
   }
 
   console.log(result);
 }
+
+numbers.forEach((number) => {
+  number.addEventListener('click', (event) => {
+    let number = event.target.dataset.value;
+
+    display.textContent = number;
+    subject = number;
+  });
+});
